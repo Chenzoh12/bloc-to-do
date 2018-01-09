516d723f31a3d69bcd25403e17c9160fa1199c37
Vincent Scotti <scottivincentj@gmail.com>
2018-01-09 20:34:28 +0000

    End of a3. React is fast.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ToDo from './components/ToDo.js';
 class App extends Component {
    constructor(props) {
      super(props);
-     
+     //this.deleteTodo= this.deleteTodo.bind(this);
      this.state = {
        todos: [
          { description: 'Walk the cat', isCompleted: true },
@@ -14,8 +14,16 @@ class App extends Component {
        ],
         newTodoDescription: ''
      };
-   }
-  
+   };
+   
+    deleteTodo(index) {
+        const todos = this.state.todos.slice();
+        const todo = todos[index];
+        const results = this.state.todos.filter(word => word != todo);
+        this.setState({ todos: results });
+        console.log(results);
+    }
+    
     handleChange(e) {
         this.setState({ newTodoDescription: e.target.value })
     }
@@ -33,13 +41,15 @@ class App extends Component {
         todo.isCompleted = todo.isCompleted ? false : true;
         this.setState({ todos: todos });
     }
-  
+    
+
+    
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) => 
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
           )}
         </ul> 
         <form onSubmit={ (e) => this.handleSubmit(e) }>
